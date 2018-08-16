d36e83e259fcaa0567059bd2c27b44ca0a3d2fb4
Olga Popova <olpop992@gmail.com>
2018-08-16 14:09:09 +0500

    Просто подправила отступы

--- js/intro.js
+++ js/intro.js
@@ -2,10 +2,10 @@
 import getElFromTemplate from './util.js';
 
 const introMarking = `
-    <section class="intro">
-        <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
-        <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
-    </section>
+<section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</section>
 `
 
 export default getElFromTemplate(introMarking);
