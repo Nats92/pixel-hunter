74c47806a48b986031d5fdec1f93328a94a0b5fe
Olga Popova <olpop992@gmail.com>
2018-08-19 14:32:50 +0500

    убран метод преобразующий строку в разметку из-за проблем с svg при использовани Template

--- js/util.js
+++ js/util.js
@@ -1,6 +1,14 @@
 
-export default (template) => {
-    const container = document.createElement(`template`);
-    container.innerHTML = template;
-    return container.content;
+export default {
+    // getElFromTemplate: (template) => {
+    //     const container = document.createElement(`template`);
+    //     container.innerHTML = template;
+    //     return container.content;
+    // },
+    clearContainer: (container) => {
+        let children = container.children;
+        for (let i = children.length - 1; i >= 0; i--) {
+            container.removeChild(children[i]);
+        }
+    }
 }
