4cb9cba6421536dbc5de77e88e577b8ca6392621
Olga Popova <olpop992@gmail.com>
2018-08-19 14:34:10 +0500

    Убрано всё лишнее из модуля и разбито по соответсвующим модулям, оставлен только вызов начального экрана игры

--- js/main.js
+++ js/main.js
@@ -1,46 +1,6 @@
-import introMarking from './intro.js';
-import greetingMarking from './greeting.js';
-// const templates = [...document.querySelectorAll(`template`)];
+import intro from './intro.js';
+import showScreen from './show-screen.js';
 
-let currScreenNumber = 0;
+intro.showScreen();
 
-const clearContainer = (container) => {
-    let children = container.children;
-    for (let i = children.length - 1; i >= 0; i--) {
-        container.removeChild(children[i]);
-    }
-};
-
-const showScreen = () => {
-    const main = document.querySelector(`#main`);
-    clearContainer(main);
-    const template = introMarking.cloneNode(true);
-    main.appendChild(template);
-};
-
-const nextScreen = (cb) => {
-    currScreenNumber++;
-    if (currScreenNumber > templates.length - 1) {
-        currScreenNumber = 0;
-    }
-    cb();
-};
-
-const prevScreen = (cb) => {
-    currScreenNumber--;
-    if (currScreenNumber < 0) {
-        currScreenNumber = templates.length - 1;
-    }
-    cb();
-};
-
-window.addEventListener(`keydown`, (evt) => {
-    if (evt.ctrlKey && evt.keyCode === 39) {
-        nextScreen(showScreen);
-    }
-    if (evt.ctrlKey && evt.keyCode === 37) {
-        prevScreen(showScreen);
-    }
-});
-
-window.addEventListener(`load`, showScreen);
+    
